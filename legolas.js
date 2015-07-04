c01214336a5d120d34dbe186a4a3ff1a2ef24990
Chris Burgess <chris@giantrobot.co.nz>
2015-07-04 17:16:32 +1200

    WIP. Now it is pushing XML docs into the website. Whee!

--- legolas.js
+++ legolas.js
@@ -10,19 +10,29 @@ const async = require('async'),
   GitHub = require('github-api'),
   shagit = require('shagit'),
   http = require('http'),
+  fse = require('fs-extra'),
   fs = require('fs'),
+  xml2js = require('xml2js'),
+  drupal = require('drupal-services-api'),
   baseURI = 'http://legislation.govt.nz',
   // crawlStart = '/subscribe/',
   /* For testing, just this year's public acts. */
   crawlStart = '/subscribe/act/public/2015',
-  downloadPath = './pdfs/',
+  downloadPath = './pdfs',
+  storeUrl = 'http://legolas.nz/rest/',
   retried = {};
 
 const crawlQueue = async.queue(crawlSearch);
 const importQueue = async.queue(importLegislation, process.env.IMPORT_QUEUE_SIZE || 1);
 const downloadQueue = async.queue(downloadLegislation, process.env.DOWNLOAD_QUEUE_SIZE || 1);
 
-crawlQueue.push(crawlStart);
+const client = new drupal('http://legolas.nz/rest');
+client.login(process.env.DRUPAL_USER, process.env.DRUPAL_PASSWORD).then(function(user) {
+  if (client.isLoggedIn()) {
+    console.log('Logged in.');
+    crawlQueue.push(crawlStart);
+  }
+});
 
 function crawlSearch(path, callback) {
   request({
@@ -60,12 +70,21 @@ function crawlSearch(path, callback) {
 
 function downloadLegislation(path, callback) {
   var m = false;
-  if (m = path.match(/.*\/(.+?\.pdf)/)) {
+  if (m = path.match(/\/subscribe\/(.*)\/(.*)\/(.*)\/(.*)\/(.*)\/(.+?\.pdf)/)) {
     var fileUrl = baseURI + path;
-    var fileName = downloadPath + m[1];
-    var file = fs.createWriteStream(fileName);
-    var request = http.get(fileUrl, function(response) {
-      response.pipe(file);
+    var fileName = m[6];
+    m = m.slice(1, 6);
+    m.unshift(downloadPath);
+    var filePath = m.join('/');
+    fse.mkdirs(filePath, function (err) {
+      if (err) return console.error(err);
+      var file = fs.createWriteStream([filePath, fileName].join('/'));
+      request.get(fileUrl)
+        .on('error', function(err) {
+          console.log(err);
+        })
+        .pipe(file);
+      //console.log(body);
       console.log('Saved ' + fileUrl + ' to ' + fileName);
     });
   }
@@ -75,8 +94,49 @@ function downloadLegislation(path, callback) {
   callback();
 }
 
-function importLegislation(task, callback) {
-  //var uri = task.uri;
-  console.log(task);
+function importLegislation(path, callback) {
+  var m = false;
+  if (m = path.match(/\/subscribe\/(.*)\/(.*)\/(.*)\/(.*)\/(.*)\/(.+?\.xml)/)) {
+    var fileUrl = baseURI + path;
+    var fileName = m[6];
+    m = m.slice(1, 6);
+    m.unshift(downloadPath);
+    var filePath = m.join('/');
+    fse.mkdirs(filePath, function (err) {
+      if (err) return console.error(err);
+      var file = fs.createWriteStream([filePath, fileName].join('/'));
+      request.get(fileUrl, function(err, response, body) {
+        client.create({
+          type: 'act',
+          title: 'Some act with body',
+          body: {
+            und: [{
+              value: body,
+              format: 'filtered_html'
+            }]
+          },
+          field_source_xml: {
+            und: [{
+              value: body
+            }]
+          }
+        }).then(function(newAct) {
+          client.retrieve(newAct.nid).then(function(retrievedAct) {
+            console.log(retrievedAct, 'new article');
+            console.log(retrievedAct.body);
+          });
+        });
+      })
+        .on('error', function(err) {
+          console.log(err);
+        })
+        .pipe(file);
+      return;
+      console.log('Saved ' + fileUrl + ' to ' + fileName);
+    });
+  }
+  else {
+    console.log('Did not recognise filename for ' + path);
+  }
   callback();
 }
